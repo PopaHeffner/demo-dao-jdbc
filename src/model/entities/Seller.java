ff386ea6b94654a41fe0af8bcba9d0cb706b8d7c
PopaHeffner <popaheffner@gmail.com>
2021-03-03 11:01:50 -0300

    Seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,18 +1,21 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 
-public class Seller implements Serializable{
+public class Seller implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Department department;
 	private Integer id;
 	private String nome;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
+
+	public Seller() {
+	}
 
 	public Seller(Department department, Integer id, String nome, String email, Date birthDate, Double baseSalary) {
 		this.department = department;
@@ -75,9 +78,7 @@ public class Seller implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
 
@@ -90,20 +91,10 @@ public class Seller implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
 			return false;
 		return true;
 	}
